0b55cfdfd613429a6da45ec5030eaf0d7158dd8f
Octavian Okumu <octaviookumu@gmail.com>
2022-04-10 12:24:08 +0300

    Fix doubleclicking functionality

--- src/app/todos/components/todo/todo.component.ts
+++ src/app/todos/components/todo/todo.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
 import { TodosService } from '../../services/todos.service';
 import { TodoInterface } from '../../types/todo.interface';
 
@@ -7,17 +7,23 @@ import { TodoInterface } from '../../types/todo.interface';
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.scss'],
 })
-export class TodoComponent implements OnInit {
+export class TodoComponent implements OnInit, OnChanges {
   @Input('todo') todoProps!: TodoInterface;
   @Input('isEditing') isEditingProps!: boolean;
   @Output('setEditingId') setEditingIdEvent: EventEmitter<string | null> =
     new EventEmitter();
+  
   editingText: string = '';
+  @ViewChild('textInput') textInput!: ElementRef;
 
   constructor(private todosService: TodosService) {}
 
   ngOnInit(): void {
     this.editingText = this.todoProps.text;
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+      console.log("changes", changes)
   }
 
   setTodoInEditMode() {
